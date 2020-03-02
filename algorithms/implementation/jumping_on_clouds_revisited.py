7b53cb5c89008a6dffa3f6b1a0b5fd6cf6522fe0
Anton Suprun <suprun.anton6@gmail.com>
2020-03-02 10:35:16 -0500

    #solutions

--- algorithms/implementation/jumping_on_clouds_revisited.py
+++ algorithms/implementation/jumping_on_clouds_revisited.py
@@ -1,2 +1,25 @@
+# e = energy (int)
+# k = jump size (int)\
+# c = array of clouds (list)
+# n = numbers of clouds
+# takes one unit of energy to make a jump
+# next cloud index = c[(i + k) % n]
+# if c[i] == 1 then e -= 2
+# game ends when lands on cloud [0]
+
+# Task: Determine final e after the game ends!
+
 def jumping_on_clouds(c, k):
-    pass
+    n = len(c)
+    e = 99
+    if c[0] == 1:
+        e -= 2
+    nextCloud = (0 + k)%n
+    while nextCloud != 0:
+        if c[nextCloud] == 1:
+            e -= 2
+        e -= 1
+        nextCloud = (nextCloud + k)%n
+    return e
+
+
--- algorithms/implementation/save_the_prisoner.py
+++ algorithms/implementation/save_the_prisoner.py
@@ -1,8 +1,14 @@
+# n = number of prisoners
+# m = the number of sweets
+# s = chair number to begin passing out treats from
+
+# Brute force: works only for small number of prisoners
+# while m != 1:
+#         if s == n:
+#             s = 0
+#         m -= 1
+#         s += 1
+#     return s
+
 def save_the_prisoner(n, m, s):
-    prisoners = n
-    candies = m
-    starting_chair = s
-    chair_number = 0
-   
-    
-        
+    # still working on it
