32603b8afd323144eee45c8687a70b8ac3ef960c
absoluteunit1 <suprun.anton6@gmail.com>
2020-03-15 12:16:42 -0400

    solution

--- algorithms/implementation/queen.py
+++ algorithms/implementation/queen.py
@@ -5,58 +5,95 @@
 #and the y position of each obstacle
 # Output: print (return) the number of squares the queen can attack from her given position 
 
+# My solution: find the distance from the Queens position to each of the objects in each direction and sum the number of moves between the queen and each object
+
 def queensAttack(n, k, r_q, c_q, obstacles):
-    numberOfMoves = 0
-    row = r_q
-    col = c_q
-    
-    # for now only works if there are no obtacles. 
-
-    # horizontal and vertical moves available
-
-    numberOfMoves += ((n-1)*2)
-    
-    # moving down-right
-    while( row != 1 and col != n ):
-        numberOfMoves += 1
-        row -= 1
-        col += 1
-
+    totalNumberOfMoves = 0
     row = r_q
     col = c_q
 
-    # moving up-right
-    while(row != n and col != n):
-        numberOfMoves += 1
-        row += 1
-        col += 1
+    moves = 0
+
+    movesRight = n - col
+    movesLeft = col -1
+    movesTop = n - row
+    movesBot = row - 1
+
+    movesBotRight =  min(n-col, row - 1) 
+    movesBotLeft = min(col-1, row - 1)
+    movesTopRight = min(n - col, n - row)
+    movesTopLeft = min(col - 1, n - row)
     
-    row = r_q
-    col = c_q
+    for obstacle in obstacles:    
 
-    # moving up-left
-    while(row != n and col != 1):
-        numberOfMoves += 1
-        row += 1
-        col -= 1
+        # checking for moves right of the queens position      
+                                                                                    
+        if obstacle[0] == row and obstacle[1] > col:         
+            if (obstacle[1] - col - 1) < movesRight:                 
+                movesRight = obstacle[1] - col - 1
+
+        # checking for moves left of the queen's position
+
+        if obstacle[0] == row and obstacle[1] < col:
+            if (col - obstacle[1] - 1) < movesLeft:
+                movesLeft = col - obstacle[1] -1 
+               
+        # checking for moves top of the queens position 
+
+        if obstacle[1] == col and obstacle[0] > row:
+            if (obstacle[0] - row - 1) < movesTop:      # checking if there are multiple objects in one direction and taking the moves to the closest one
+                movesTop = obstacle[0] - row - 1
+
+        # checking for moves below the queens position 
+
+        if obstacle[1] == col and obstacle[0] < row:
+            if (row - obstacle[0] - 1) < movesBot:      # checking if there are multiple objects in one direction and taking the moves to the closest one
+                movesBot = row - obstacle[0] - 1
+
+        # DIAGONALLY
+
+        # bottom - right
+
+        while( ( row != 1 and col != n ) and (row - 1 != obstacle[0]  and col + 1 != obstacle[1] )):
+            col += 1
+            row -= 1
+            moves += 1    
+        if moves < movesBotRight:
+            movesBotRight = moves
+        moves = 0
+
+        # bottom - left 
+
+        while( ( row != 1 and col != 1 ) and (row - 1 != obstacle[0]  and col - 1 != obstacle[1] )):
+            col -= 1
+            row -= 1
+            moves += 1    
+        if moves < movesBotLeft:
+            movesBotLeft = moves
+        moves = 0
+
+        # top - right
+
+        while( ( row != n and col != n ) and (row + 1 != obstacle[0]  and col + 1 != obstacle[1]  )):
+            col += 1
+            row += 1
+            moves += 1    
+        if moves < movesTopRight:
+            movesTopRight = moves
+        moves = 0
+
+        # top - left
+
+        while( ( row != n and col != 1 ) and (row + 1 != obstacle[0]   and col - 1 != obstacle[1] )):
+            col -= 1
+            row += 1
+            moves += 1    
+        if moves < movesTopLeft:
+            movesTopLeft = moves
+        moves = 0
     
-    row = r_q
-    col = c_q
-
-    # moving down-left
-    while(row != 1 and col != 1):
-        numberOfMoves += 1
-        row -= 1
-        col -= 1
+    totalNumberOfMoves = movesBot + movesBotLeft + movesBotRight + movesLeft + movesTopLeft + movesTop + movesTopRight + movesRight
     
-    row = r_q
-    col = c_q
+    return totalNumberOfMoves
 
-    return numberOfMoves
-
-# Expect 9
-print(queensAttack(4, None, 4, 4, None))
-
-# Expect 14
-print(queensAttack(5, None, 4, 4, None))
-
+print(queensAttack(5, 3, 4, 3, [(5,5), (4,2), (2,3)] ))
