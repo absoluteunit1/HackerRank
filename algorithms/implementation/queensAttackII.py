d7a894472cca13d1f0e003943395cb44bd1cf219
absoluteunit1 <suprun.anton6@gmail.com>
2020-03-19 09:00:15 -0400

    fixed solution and studied it.

--- algorithms/implementation/queensAttackII.py
+++ algorithms/implementation/queensAttackII.py
@@ -1,27 +1,65 @@
+# Could not get my solution to pass all tests.
+# My original solution used many while loops and as a result was not efficient and would take a long time to run
+# This solution is based on the solution in the discussion sections
+
 def queensAttack(n, k, r_q, c_q, obstacles):
-    u = n - r_q
-    d = r_q-1
-    r = n - c_q
-    l = c_q-1
-    ru = min(u, r)
-    rd = min(r,d)
-    lu = min(l,u)
-    ld = min(l,d)
-    for o in obstacles:
-        if o[1] == c_q:
-            if o[0] < r_q:
-                d = min(d, r_q-1-o[0])
+
+    up = n - r_q
+    down = r_q - 1
+    right = n - c_q
+    left = c_q - 1
+
+    rightUp = min(up, right)
+    rightDown = min(right,down)
+    leftUP = min(left,up)
+    leftDown = min(left,down)
+
+    # iterate through each obstacle in the obstacles list of tuples
+    for obstacle in obstacles:
+
+        # checks if the obstacle is in the same column as the queen
+        if obstacle[1] == c_q:
+
+            # this if/else block checks to see if the obstacle is below the queen or above. Gives the minimum number of moves to the end of board or to the next obstacle. 
+            if obstacle[0] < r_q:
+                down = min(down, r_q-1-obstacle[0])
+
             else:
-                u = min(u, o[0]-r_q-1)
-        elif o[0] == r_q:
-            if o[1] < c_q: l = min(l, c_q-1-o[1])
-            else: r = min(r, o[1]-c_q-1)
-        elif abs(o[0]-r_q) == abs(o[1]-c_q):
-            if o[1]>c_q:
-                if o[0]>r_q: ru = min(ru, o[1]-c_q-1)
-                else: rd = min(rd, o[1]-c_q-1)
+                up = min(up, obstacle[0]-r_q-1)
+
+        # checks if the obstacle is in the same row as the queen
+        elif obstacle[0] == r_q:
+
+            # this if/else block checks to see if the obstacle is left or right of the queen. Gives the minimum numbers of moves; either to end of board or to next obstacle in the row
+            if obstacle[1] < c_q: 
+                left = min(left, c_q-1-obstacle[1])
+
+            else: 
+                right = min(right, obstacle[1]-c_q-1)
+
+        # checks if the difference between the obstacle row/queen row and obstacle column/queen column is the same. If so, means that the obstacle is in the same diagonal as the queen
+        elif abs(obstacle[0]-r_q) == abs(obstacle[1]-c_q):
+
+            # checks if the obstacle column is right of the queen column (checking to see if the diagonal obstacle is to the right or left of the queen )
+            if obstacle[1]>c_q:
+
+                # this if/else block checks if the obstacle row is above or below the queen row (checking to see if the diagonal is right up or right down from the queen).  Takes the minimum of either: the diagonal end of board or the number of moves to the column where the obstacle is
+                if obstacle[0]>r_q: 
+                    rightUp = min(rightUp, obstacle[1]-c_q-1)
+
+                else: 
+                    rightDown = min(rightDown, obstacle[1]-c_q-1)
+
+            # this else block accounts for the obstacle being to the left of the queen column.
             else:
-                if o[0]>r_q: lu = min(lu, c_q-1-o[1])
-                else: ld = min(ld, c_q-1-o[1])
-                
-    return u + d + r + l + ru + rd + lu + ld
+
+                # this if/else block accounts for the obstacle being up or down from the queen. Takes the minimum of either: the diagonal end of board or the number of moves to the column where the obstacle is
+                if obstacle[0]>r_q: 
+                    leftUP = min(leftUP, c_q-1-obstacle[1])
+
+                else: 
+                    leftDown = min(leftDown, c_q-1-obstacle[1])
+    
+    # returns the total of all the moves
+    
+    return up + down + right + left + rightUp + rightDown + leftUP + leftDown
