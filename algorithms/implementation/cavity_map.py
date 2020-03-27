1986b4b3b0dd125593d06526b38a5c38d1edabe5
absoluteunit1 <suprun.anton6@gmail.com>
2020-03-27 11:47:49 -0400

    solution

--- algorithms/implementation/cavity_map.py
+++ algorithms/implementation/cavity_map.py
@@ -1,5 +1,11 @@
-def cavityMap(grid):
-    for i in range(len(grid)):
-        for j in range(len(i)):
-            pass
-# finish another time!
+def cavityMap(n, grid):
+    for i in range(1,n-1):
+        for j in range(1,n-1):
+            if grid[i][j] > max(grid[i-1][j],grid[i+1][j],grid[i][j-1],grid[i][j+1]):    # this comparison works because "X">"9">"1". Strings are considered higher than digits ("9") in Python. 
+                grid[i] = grid[i][:j] +'X' + grid[i][j+1:]    # Intersting way to modify a string, slice (excluding the char you are replacing) and add the char you are replacing it with in the middle of the slices        
+    return grid
+
+
+
+        
+
